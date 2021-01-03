e964766123effd12778e5044451fb8c7336cdff8
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2021-01-03 16:10:29 +0800

    update deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :repo_url, 'git@github.com:lctseng/gdrive-assistant.git'
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/home/lctseng_apps/deploy/gdrive-assistant'
+set :deploy_to, '/home/app/deploy/gdrive-assistant'
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
